9fe3df14c601116efa24336b8b3076356f0dc204
Mathilde Renard <mathilderenard626@gmail.com>
2021-11-04 12:18:30 +0100

    Généralisation

--- TP/Assets/Scripts/RotateDuringKeyPressed.cs
+++ TP/Assets/Scripts/RotateDuringKeyPressed.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RotateDuringKeyPressed : MonoBehaviour
 {
+    public string key;
     public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
@@ -14,7 +15,7 @@ public class RotateDuringKeyPressed : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("r"))
+        if (Input.GetKey(key))
         {
             transform.Rotate(Vector3.right * speed * Time.deltaTime, 5);
             print("Mouvement effectué.");
